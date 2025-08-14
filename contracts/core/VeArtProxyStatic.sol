3165070eacc1c88f5c6db5b40410da1a16c8ff96
Aegas Dev <support@aegas.io>
2025-08-14 13:23:53 +0300

    update svg view

--- contracts/core/VeArtProxyStatic.sol
+++ contracts/core/VeArtProxyStatic.sol
@@ -8,14 +8,6 @@ import "./interfaces/IVeArtProxyStatic.sol";
  * @notice A contract that manages art representation of locked and unlocked veNFT states.
  */
 contract VeArtProxyStatic is IVeArtProxyStatic {
-    /// @notice Icon representing the locked state.
-    string public lockedIcon;
-    /// @notice Icon representing the unlocked state.
-    string public unlockedIcon;
-    /// @notice Representation of a transferable part.
-    string public transferablePart;
-    /// @notice Representation of a non-transferable part.
-    string public notTransferablePart;
     /// @inheritdoc IVeArtProxyStatic
     string public override endPart;
     /// @inheritdoc IVeArtProxyStatic
@@ -24,25 +16,6 @@ contract VeArtProxyStatic is IVeArtProxyStatic {
     bool public setupStart;
     /// @notice Boolean indicating whether the end part has been set up.
     bool public setupEnd;
-
-    /**
-     * @notice Constructor to initialize the VeArtProxyStatic contract.
-     * @param lockedIcon_ Icon for locked state.
-     * @param unlockedIcon_ Icon for unlocked state.
-     * @param transferablePart_ Part that is transferable.
-     * @param notTransferablePart_ Part that is not transferable.
-     */
-    constructor(
-        string memory lockedIcon_,
-        string memory unlockedIcon_,
-        string memory transferablePart_,
-        string memory notTransferablePart_
-    ) {
-        lockedIcon = lockedIcon_;
-        unlockedIcon = unlockedIcon_;
-        transferablePart = transferablePart_;
-        notTransferablePart = notTransferablePart_;
-    }
 
     /**
      * @notice Sets the starting part of the veNFT art.
@@ -64,23 +37,5 @@ contract VeArtProxyStatic is IVeArtProxyStatic {
         require(!setupEnd, "Already setup");
         endPart = endPart_;
         setupEnd = true;
-    }
-
-    /**
-     * @notice Returns the appropriate part depending on the transferability status.
-     * @param isTransferable_ Boolean indicating if the part is transferable.
-     * @return The corresponding transferable or non-transferable part.
-     */
-    function getIsTransferablePart(bool isTransferable_) external view returns (string memory) {
-        return isTransferable_ ? transferablePart : notTransferablePart;
-    }
-
-    /**
-     * @notice Returns the appropriate icon depending on the lock status.
-     * @param isLock_ Boolean indicating if the state is locked.
-     * @return The corresponding locked or unlocked icon.
-     */
-    function getLockIcon(bool isLock_) external view returns (string memory) {
-        return isLock_ ? lockedIcon : unlockedIcon;
     }
 }
--- contracts/core/interfaces/IVeArtProxyStatic.sol
+++ contracts/core/interfaces/IVeArtProxyStatic.sol
@@ -4,9 +4,5 @@ pragma solidity >=0.8.0;
 interface IVeArtProxyStatic {
     function endPart() external view returns (string memory);
 
-    function getLockIcon(bool isLock_) external view returns (string memory);
-
-    function getIsTransferablePart(bool isTransferable_) external view returns (string memory);
-
     function startPart() external view returns (string memory);
 }
