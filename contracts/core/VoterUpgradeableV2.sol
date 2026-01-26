4b8f03844541a4c4178a5c18a497f28a53967c18
Alison024 <andreo.ublin24@gmail.com>
2026-01-26 12:52:52 +0200

    fixed comment  #5

--- contracts/core/VoterUpgradeableV2.sol
+++ contracts/core/VoterUpgradeableV2.sol
@@ -403,10 +403,9 @@ contract VoterUpgradeableV2 is IVoter, AccessControlUpgradeable, ReentrancyGuard
 
     function notifyRewardAmount(uint256 amount_) external {
         _checkSender(minter);
-
-        IERC20Upgradeable(token).safeTransferFrom(_msgSender(), address(this), amount_);
         uint256 weightAt = totalWeightsPerEpoch[epochTimestamp() - _WEEK]; // minter call notify after updates active_period, loads votes - 1 week
         if (weightAt > 0) {
+            IERC20Upgradeable(token).safeTransferFrom(_msgSender(), address(this), amount_);
             index += (amount_ * 1e18) / weightAt;
         }
         emit NotifyReward(_msgSender(), token, amount_);
